0d8e9ec82aa095ed0a9ba052da9882ae46a71164
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-09-13 14:57:03 +0200

    add type and abilities to card pokemon

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="MainCards">
     <div class="Cards">
-      <p>{{ pokemons }}</p>
-      <p>{{ weight }} weight</p>
-      <p>{{ height }} height</p>
-      <p>Types :</p>
-      <span v-for="(type, i) in types" :key="i"> | {{ type.type.name }} | </span>
+      <div class="Cards__poke">
+        <h2>{{ pokemons }}</h2>
+        <img class="Cards__img" :src='img.front_default' :alt="pokemons">
+      </div>
+      <div class="Cards__values">
+        <div class="Cards__value">
+          <p>{{ weight }} weight</p>
+          <p>{{ height }} height</p>
+          <span class="Cards__type" v-for="(type, i) in types" :key="i">{{ type.type.name }}</span>
+        </div>
+        <div class="Cards__abilities" v-for="(ability, i) in abilities" :key="i">
+          <p class="Cards__ability">{{ ability.ability.name }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +29,8 @@ export default {
     weight: '',
     height: '',
     types: {},
+    img: '',
+    abilities: {},
   }),
   created() {
     getPokemon()
@@ -28,6 +39,8 @@ export default {
         this.weight = data.weight;
         this.height = data.height;
         this.types = data.types;
+        this.img = data.sprites;
+        this.abilities = data.abilities;
         console.log(data);
       });
   },
@@ -43,12 +56,51 @@ export default {
   }
 
   .Cards{
+    display: flex;
+    justify-content: space-evenly;
     padding: 1rem;
-    width: 300px;
+    width: 70%;
+    height: 50%;
     background-color: rgb(119, 35, 35);
     color: rgb(255, 255, 255);
     margin-left: 10px;
     margin-bottom: 10px;
     border-radius: 10px;
+  }
+
+  .Cards__values{
+    width: 60%;
+  }
+
+  .Cards__value{
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .Cards__abilities{
+    height: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .Cards__ability{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: lightslategrey;
+    width: 300px;
+    height: 80px;
+  }
+
+  .Cards__img{
+    width: 200px;
+  }
+
+  .Cards__type{
+    width: 60px;
+    background-color: lightslategrey;
+    padding: .7rem;
+    border-radius: 5px;
   }
 </style>
